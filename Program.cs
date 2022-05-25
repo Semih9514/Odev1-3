77c8985c0e7d73c3bfe3ff5d2bbddf3e8c423039
semih.bagdas <semih.bagdas@gmail.com>
2022-05-26 01:51:13 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Odev1_3
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+           Console.Write("Kaç tane kelime gireceksiniz:");
+           int sayi = Int32.Parse(Console.ReadLine());
+           string[] dizi = new string[sayi];
+           Console.WriteLine("---------------------------------");
+           for (int i = 0; i < sayi; i++)
+           {
+               Console.Write("{0}. Kelime Giriniz:",i+1);
+               dizi[i]=Console.ReadLine();
+           }
+           Console.WriteLine("---------------------------------");
+           Console.WriteLine("Kelimelerin Ters Çevrilmiş Halleri");
+           Console.WriteLine("---------------------------------");
+           foreach (var item in dizi)
+           {
+               for (int i = item.Length; i >0; i--)
+               {
+                   Console.Write(item.Substring(i-1, 1).ToString());
+               }
+               Console.WriteLine();
+           }
+        }
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,1 @@
+<h2>Bir konsol uygulamasında kullanıcıdan pozitif bir sayı girmesini isteyin (n). Sonrasında kullanıcıdan n adet kelime girmesi isteyin. Kullanıcının girişini yaptığı kelimeleri sondan başa doğru console'a yazdırın.<h2>
